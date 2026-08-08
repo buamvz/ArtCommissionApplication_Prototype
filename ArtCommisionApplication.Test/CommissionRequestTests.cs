9ed456a7e25971ad89eaa06d9e32d488a3bc3415
buamvz <lovebrooke321@gmail.com>
2026-08-08 22:03:02 +1200

    Brooke Stone - 08/08/26 - 3
    Added commission request model and 5 validation tests (All test pass), also changed MSTest Version to the one TA showed in leacture

--- ArtCommisionApplication.Test/CommissionRequestTests.cs
+++ ArtCommisionApplication.Test/CommissionRequestTests.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ArtCommisionApplication;
+﻿using ArtCommisionApplication;
+using ArtCommissionApplication_Prototype;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 
 namespace ArtCommisionApplication.Test
@@ -8,8 +9,41 @@ namespace ArtCommisionApplication.Test
     public class CommissionRequestTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CommissionRequest_EmptyClientName_ThrowsException()
         {
+            Assert.ThrowsException<ArgumentException>(() =>
+                new CommissionRequest("C001", "", "mahitohater67@gmail.com", "Half Body drawing", "Me and Junpei having a picnic", 100m));
         }
-    }
+
+        [TestMethod]
+        public void CommissionRequest_EmptyClientEmail_ThrowsException()
+        {
+            Assert.ThrowsException<ArgumentException>(() =>
+                new CommissionRequest("C001", "Yuji", "", "Half Body drawing", "Me and Junpei having a picnic", 100m));
+        }
+
+        [TestMethod]
+        public void CommissionRequest_NegativePrice_ThrowsException()
+        {
+            Assert.ThrowsException<ArgumentException>(() =>
+                new CommissionRequest("C001", "Yuji", "mahitohater67@gmail.com", "Half Body drawing", "Me and Junpei having a picnic", -50m));
+        }
+
+
+        [TestMethod]
+        public void CommissionRequest_ValidDetails_CreatesPendingRequest()
+        {
+            var request = new CommissionRequest("C001", "Yuji", "mahitohater67@gmail.com", "Half Body drawing", "Me and Junpei having a picnic", 100m);
+
+            Assert.AreEqual(CommissionStatus.Pending, request.Status);
+        }
+
+        [TestMethod]
+        public void CommissionRequest_EmptyDescription_ThrowsException()
+        {
+            Assert.ThrowsException<ArgumentException>(() =>
+                new CommissionRequest("C001", "Yuji", "mahitohater67@gmail.com", "Half Body drawing", "", 100m));
+
+        }
+     }
 }
--- ArtCommissionApplication_Prototype/CommissionRequest.cs
+++ ArtCommissionApplication_Prototype/CommissionRequest.cs
@@ -6,15 +6,50 @@ namespace ArtCommissionApplication_Prototype
 {
     public class CommissionRequest
     {
+        //having majority as get without set so that the program cannot change as theyre set in the constructor
         public string Id { get; }
         public string ClientName { get; }
         public string ClientEmail { get; }
-        public string CommisionType { get; }
-        public string Discription { get; }
-        public string EstimatedPrice { get; }
-        public CommissionStatus Status { get; set; }
-        public string SubmittedDate { get; }
+        public string CommissionType { get; }
+        public string Description { get; }
+        //decimal for estimated price as ita representing money $$$
+        public decimal EstimatedPrice { get; }
+        //private set for commision status will be used to update the status of the commission request
+        //cant alow invaild transitions in status
+        public CommissionStatus Status { get; private set; }
+        public DateTime SubmittedDate { get; }
 
+        public CommissionRequest(string id, string clientName, string clientEmail, string commissionType, string description, decimal estimatedPrice)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+                throw new ArgumentException("Commission ID is required");
 
+            if (string.IsNullOrWhiteSpace(clientName))
+                throw new ArgumentException("Clients name is required");
+
+            if (string.IsNullOrWhiteSpace(clientEmail))
+                throw new ArgumentException("Clients email is required");
+
+            if (string.IsNullOrWhiteSpace(commissionType))
+                throw new ArgumentException("Commission type is required");
+
+            if (string.IsNullOrWhiteSpace(description))
+                throw new ArgumentException("A description for the commission is required");
+
+            if (estimatedPrice < 0)
+                throw new ArgumentException("Estimated price cannot be negative");
+
+            //initialising properties with values needed
+            Id = id;
+            ClientName = clientName;
+            ClientEmail = clientEmail;
+
+            CommissionType = commissionType;
+            Description = description;
+            EstimatedPrice = estimatedPrice;
+
+            Status = CommissionStatus.Pending;
+            SubmittedDate = DateTime.Now;
+        }
     }
 }
