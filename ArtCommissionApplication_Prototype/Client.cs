734a21284b4bf62a731dc7f62e11cc8ce049847e
SiennaHa <sienna.hargreaves31@gmail.com>
2026-08-11 16:03:30 +1200

    Client and Commission Type Classes

---
+++ ArtCommissionApplication_Prototype/Client.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArtCommissionApplication_Prototype
+{
+    public class Client
+    {
+        public string Id { get; }
+        public string ClientName { get; }
+        public string ClientEmail { get; }
+
+        public Client(string id, string clientName, string email) 
+        {
+            if (string.IsNullOrWhiteSpace(id))
+                throw new ArgumentException("Commission ID is required.");
+
+            if (string.IsNullOrWhiteSpace(clientName))
+                throw new ArgumentException("Clients name is required.");
+
+            if (string.IsNullOrWhiteSpace(email))
+                throw new ArgumentException("Clients email is required.");
+
+            Id = id;
+            ClientName = clientName;
+            ClientEmail = email;
+        }
+        
+    }
+}
--- ArtCommissionApplication_Prototype/CommissionRequest.cs
+++ ArtCommissionApplication_Prototype/CommissionRequest.cs
@@ -7,11 +7,15 @@ namespace ArtCommissionApplication_Prototype
     public class CommissionRequest
     {
         //having majority as get without set so that the program cannot change as theyre set in the constructor
-        public string Id { get; }
-        public string ClientName { get; }
-        public string ClientEmail { get; }
-        public string CommissionType { get; }
+
+        // sienna - adding classes for client and commission type
+        public Client Client { get;  }
+
+        public CommissionType CommissionType { get; }
+        // public string CommissionType { get; }
         public string Description { get; }
+
+
         //decimal for estimated price as ita representing money $$$
         public decimal EstimatedPrice { get; }
         //private set for commision status will be used to update the status of the commission request
@@ -19,32 +23,25 @@ namespace ArtCommissionApplication_Prototype
         public CommissionStatus Status { get; private set; }
         public DateTime SubmittedDate { get; }
 
-        public CommissionRequest(string id, string clientName, string clientEmail, string commissionType, string description, decimal estimatedPrice)
+        public CommissionRequest(Client client, CommissionType commissionType, string description, decimal estimatedPrice)
         {
-            if (string.IsNullOrWhiteSpace(id))
-                throw new ArgumentException("Commission ID is required");
+            Client = client ?? throw new ArgumentNullException(nameof(client));
+            CommissionType = commissionType ?? throw new ArgumentNullException(nameof(commissionType));
 
-            if (string.IsNullOrWhiteSpace(clientName))
-                throw new ArgumentException("Clients name is required");
-
-            if (string.IsNullOrWhiteSpace(clientEmail))
-                throw new ArgumentException("Clients email is required");
-
-            if (string.IsNullOrWhiteSpace(commissionType))
-                throw new ArgumentException("Commission type is required");
+            //if (string.IsNullOrWhiteSpace(commissionType))
+            //    throw new ArgumentException("Commission type is required.");
 
             if (string.IsNullOrWhiteSpace(description))
-                throw new ArgumentException("A description for the commission is required");
+                throw new ArgumentException("A description for the commission is required.");
 
             if (estimatedPrice < 0)
-                throw new ArgumentException("Estimated price cannot be negative");
+                throw new ArgumentException("Estimated price cannot be negative.");
 
             //initialising properties with values needed
-            Id = id;
-            ClientName = clientName;
-            ClientEmail = clientEmail;
 
-            CommissionType = commissionType;
+            //Client = client;
+            //CommissionType = commissionType;
+
             Description = description;
             EstimatedPrice = estimatedPrice;
 
