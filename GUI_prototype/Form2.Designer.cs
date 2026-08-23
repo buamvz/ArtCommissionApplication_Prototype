941411c9b50d61c355c210bcc6947f0d1b5037e2
buamvz <lovebrooke321@gmail.com>
2026-08-23 15:19:00 +1200

    Brooke 23/08/26
    Adding check boxes in GUI

--- GUI_prototype/Form2.Designer.cs
+++ GUI_prototype/Form2.Designer.cs
@@ -28,69 +28,219 @@
         /// </summary>
         private void InitializeComponent()
         {
-            panel1 = new Panel();
-            label1 = new Label();
-            button1 = new Button();
-            textBox3 = new TextBox();
             label2 = new Label();
+            textBox3 = new TextBox();
+            SubmitRequestButton = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            checkBox1 = new CheckBox();
+            label4 = new Label();
+            checkBox10 = new CheckBox();
+            label5 = new Label();
+            checkBox9 = new CheckBox();
+            checkBox8 = new CheckBox();
+            checkBox7 = new CheckBox();
+            checkBox6 = new CheckBox();
+            checkBox2 = new CheckBox();
+            panel1 = new Panel();
+            checkBox3 = new CheckBox();
             panel1.SuspendLayout();
             SuspendLayout();
-            // 
-            // panel1
-            // 
-            panel1.AutoScroll = true;
-            panel1.Controls.Add(label1);
-            panel1.Controls.Add(button1);
-            panel1.Controls.Add(textBox3);
-            panel1.Location = new Point(12, 77);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(776, 361);
-            panel1.TabIndex = 4;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(11, 13);
-            label1.Name = "label1";
-            label1.Size = new Size(39, 15);
-            label1.TabIndex = 3;
-            label1.Text = "Email:";
-            label1.Click += label1_Click;
-            // 
-            // button1
-            // 
-            button1.Location = new Point(569, 328);
-            button1.Name = "button1";
-            button1.Size = new Size(159, 71);
-            button1.TabIndex = 0;
-            button1.Text = "Request Commission";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // textBox3
-            // 
-            textBox3.Location = new Point(11, 31);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(365, 23);
-            textBox3.TabIndex = 2;
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.BackColor = Color.FromArgb(192, 192, 255);
-            label2.Font = new Font("SWItal", 32.25F, FontStyle.Italic, GraphicsUnit.Point, 0);
-            label2.Location = new Point(12, 9);
+            label2.Font = new Font("Microsoft Sans Serif", 32.25F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label2.Location = new Point(14, 12);
             label2.Name = "label2";
-            label2.Size = new Size(518, 55);
+            label2.Size = new Size(493, 63);
             label2.TabIndex = 5;
             label2.Text = "Commission Forms";
             // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(13, 41);
+            textBox3.Margin = new Padding(3, 4, 3, 4);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(417, 27);
+            textBox3.TabIndex = 2;
+            // 
+            // SubmitRequestButton
+            // 
+            SubmitRequestButton.Location = new Point(26, 562);
+            SubmitRequestButton.Margin = new Padding(3, 4, 3, 4);
+            SubmitRequestButton.Name = "SubmitRequestButton";
+            SubmitRequestButton.Size = new Size(182, 95);
+            SubmitRequestButton.TabIndex = 0;
+            SubmitRequestButton.Text = "Submit Request";
+            SubmitRequestButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(13, 17);
+            label1.Name = "label1";
+            label1.Size = new Size(52, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Name:";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(13, 111);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(417, 27);
+            textBox1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(13, 87);
+            label3.Name = "label3";
+            label3.Size = new Size(49, 20);
+            label3.TabIndex = 5;
+            label3.Text = "Email:";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(23, 215);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(59, 24);
+            checkBox1.TabIndex = 7;
+            checkBox1.Text = "Bust";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(13, 177);
+            label4.Name = "label4";
+            label4.Size = new Size(144, 20);
+            label4.TabIndex = 8;
+            label4.Text = "Type of commission:";
+            label4.Click += label4_Click;
+            // 
+            // checkBox10
+            // 
+            checkBox10.AutoSize = true;
+            checkBox10.CheckAlign = ContentAlignment.BottomCenter;
+            checkBox10.Location = new Point(28, 394);
+            checkBox10.Name = "checkBox10";
+            checkBox10.Size = new Size(21, 41);
+            checkBox10.TabIndex = 13;
+            checkBox10.Text = "1";
+            checkBox10.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(13, 356);
+            label5.Name = "label5";
+            label5.Size = new Size(263, 20);
+            label5.TabIndex = 14;
+            label5.Text = "Number of Characters for commission:";
+            // 
+            // checkBox9
+            // 
+            checkBox9.AutoSize = true;
+            checkBox9.CheckAlign = ContentAlignment.BottomCenter;
+            checkBox9.Location = new Point(83, 394);
+            checkBox9.Name = "checkBox9";
+            checkBox9.Size = new Size(21, 41);
+            checkBox9.TabIndex = 15;
+            checkBox9.Text = "2";
+            checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            checkBox8.AutoSize = true;
+            checkBox8.CheckAlign = ContentAlignment.BottomCenter;
+            checkBox8.Location = new Point(136, 394);
+            checkBox8.Name = "checkBox8";
+            checkBox8.Size = new Size(21, 41);
+            checkBox8.TabIndex = 16;
+            checkBox8.Text = "3";
+            checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            checkBox7.AutoSize = true;
+            checkBox7.CheckAlign = ContentAlignment.BottomCenter;
+            checkBox7.Location = new Point(187, 394);
+            checkBox7.Name = "checkBox7";
+            checkBox7.Size = new Size(21, 41);
+            checkBox7.TabIndex = 17;
+            checkBox7.Text = "4";
+            checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            checkBox6.AutoSize = true;
+            checkBox6.CheckAlign = ContentAlignment.BottomCenter;
+            checkBox6.Location = new Point(242, 394);
+            checkBox6.Name = "checkBox6";
+            checkBox6.Size = new Size(21, 41);
+            checkBox6.TabIndex = 18;
+            checkBox6.Text = "5";
+            checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(23, 255);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(97, 24);
+            checkBox2.TabIndex = 19;
+            checkBox2.Text = "Half Body";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.AutoScroll = true;
+            panel1.Controls.Add(checkBox3);
+            panel1.Controls.Add(checkBox2);
+            panel1.Controls.Add(checkBox6);
+            panel1.Controls.Add(checkBox7);
+            panel1.Controls.Add(checkBox8);
+            panel1.Controls.Add(checkBox9);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(checkBox10);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(checkBox1);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(SubmitRequestButton);
+            panel1.Controls.Add(textBox3);
+            panel1.Location = new Point(14, 103);
+            panel1.Margin = new Padding(3, 4, 3, 4);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(887, 684);
+            panel1.TabIndex = 4;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(23, 298);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(92, 24);
+            checkBox3.TabIndex = 20;
+            checkBox3.Text = "Full Body";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
             // Form2
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(914, 800);
             Controls.Add(label2);
             Controls.Add(panel1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form2";
             Text = "Form2";
             panel1.ResumeLayout(false);
@@ -100,10 +250,22 @@
         }
 
         #endregion
-        private Panel panel1;
-        private Button button1;
-        private Label label1;
-        private TextBox textBox3;
         private Label label2;
+        private TextBox textBox3;
+        private Button SubmitRequestButton;
+        private Label label1;
+        private TextBox textBox1;
+        private Label label3;
+        private CheckBox checkBox1;
+        private Label label4;
+        private CheckBox checkBox10;
+        private Label label5;
+        private CheckBox checkBox9;
+        private CheckBox checkBox8;
+        private CheckBox checkBox7;
+        private CheckBox checkBox6;
+        private CheckBox checkBox2;
+        private Panel panel1;
+        private CheckBox checkBox3;
     }
 }
--- GUI_prototype/Form2.cs
+++ GUI_prototype/Form2.cs
@@ -24,5 +24,25 @@ namespace GUI_prototype
         {
 
         }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
